d94f6c80739851cae03fa529caee9d348d63cf95
erdogant <erdogant@gmail.com>
2020-04-05 00:22:49 +0200

    new version

--- hnet/__init__.py
+++ hnet/__init__.py
@@ -10,9 +10,14 @@ from hnet.hnet import (
 	import_example,
 )
 
+from hnet.utils.adjmat_vec import (
+    vec2adjmat,
+    adjmat2vec,
+    )
+
 __author__ = 'Erdogan Tasksen'
 __email__ = 'erdogant@gmail.com'
-__version__ = '0.1.7'
+__version__ = '0.1.8'
 
 # module level doc-string
 __doc__ = """
@@ -21,24 +26,17 @@ HNET - Graphical Hypergeometric networks.
 
 Description
 -----------
-Creating networks from datasets with mixed datatypes return
-by an unknown function. These datasets can range from generic dataframes to
-nested data structures with lists, missing values and enumerations.
-I solved this problem to minimize the amount of configurations required
-while still gaining many benefits of having schemas available.
-
-The response variable (y) should be a vector with the same number of samples
-as for the input data. For each column in the dataframe significance is
-assessed for the labels in a two-class approach (y=1 vs y!=1).
-Significane is assessed one tailed; only the fit for y=1 with an
-overrepresentation. Hypergeometric test is used for catagorical values
-Wilcoxen rank-sum test for numerical values.
+Creation of networks from datasets with mixed datatypes and with unknown function. 
+Input datasets can range from generic dataframes to nested data structures with lists, missing values and enumerations.
+HNet (graphical Hypergeometric Networks) is a method where associations across variables are tested for significance by statistical inference.
+The aim is to determine a network with significant associations that can shed light on the complex relationships across variables.
 
 
 Example
 -------
 
 >>> import hnet
+>>> df = hnet.import_example('student')
 >>> model = hnet.fit(df)
 >>> G = hnet.plot(model)
 >>> G = hnet.heatmap(model)

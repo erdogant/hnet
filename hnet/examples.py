f1a3a681ea0542a66ad4e8d68d11de81c5c65cf0
erdogant <erdogant@gmail.com>
2020-04-05 00:23:00 +0200

    new examples

--- hnet/examples.py
+++ hnet/examples.py
@@ -1,15 +1,23 @@
 import hnet
 print(hnet.__version__)
+print(dir(hnet))
 
 
 # %%
-df = hnet.import_example()
+df = hnet.import_example('student')
+
+df = hnet.import_example('titanic')
+
+df = hnet.import_example('sprinkler')
+
+
+# %%
 
 model = hnet.fit(df)
 
 G = hnet.plot(model)
 
-G = hnet.heatmap(model)
+G = hnet.heatmap(model, cluster=True)
 
 G = hnet.d3graph(model)
 
@@ -18,3 +26,9 @@ G = hnet.d3graph(model)
 rules = hnet.combined_rules(model)
 
 adjmatSymmetric = hnet.to_symmetric(model)
+
+# %% Enrichment
+
+df = hnet.import_example('titanic')
+y = df['Survived'].values
+out = hnet.enrichment(df, y)

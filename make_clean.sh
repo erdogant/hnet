e5c250dd98eb60da242ac809e404cce53b6f9bfd
Erdogan Taskesen <erdogant@gmail.com>
2020-10-04 19:39:26 +0200

    cleaning library files

--- make_clean.sh
+++ make_clean.sh
@@ -5,7 +5,13 @@ rm -rf hnet.egg-info
 rm -rf __pycache__
 rm -rf .pytest_cache
 rm -rf .pylint.d
+rm -rf hnet/.pylint.d
+rm -rf hnet/__pycache__
+rm -rf hnet/utils/__pycache__
+rm -rf tests/__pycache__
 rm -rf hnet/data/*.zip
+rm -rf hnet/data/*.data
+rm -rf hnet/data/*.names
 rm -rf *.js
 rm -rf *.html
 rm -rf *.css

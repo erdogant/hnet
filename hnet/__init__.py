fe63a507511c358eb56d43431349e57fb02a1c96
erdogant <erdogant@gmail.com>
2022-02-28 02:00:52 +0100

    1.1.11

--- hnet/__init__.py
+++ hnet/__init__.py
@@ -14,7 +14,7 @@ from hnet.adjmat_vec import (
 
 __author__ = 'Erdogan Tasksen'
 __email__ = 'erdogant@gmail.com'
-__version__ = '1.1.10'
+__version__ = '1.1.11'
 
 # module level doc-string
 __doc__ = """
